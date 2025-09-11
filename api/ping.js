e5e715ab61d22e1b74d5cd41de717050f9206782
jonassercombe <112658657+jonassercombe@users.noreply.github.com>
2025-09-11 11:12:10 +0200

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,6 +1,4 @@
 export default async function handler(req, res) {
   return res.status(200).send("ok");
 }
-
-// Erzwinge Node-Runtime, falls Vercel Edge defaultet:
-export const config = { runtime: "nodejs18.x" };
+export const config = { runtime: "nodejs" };
