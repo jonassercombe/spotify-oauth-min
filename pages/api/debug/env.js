31a80d9f4bda2d1745011d749cabcb2d46175508
jonassercombe <112658657+jonassercombe@users.noreply.github.com>
2025-09-13 17:05:49 +0200

    Create env.js

---
+++ pages/api/debug/env.js
@@ -0,0 +1,7 @@
+export default async function handler(req, res) {
+  return res.status(200).json({
+    SUPABASE_URL: !!process.env.SUPABASE_URL,
+    SUPABASE_SERVICE_ROLE_KEY: !!process.env.SUPABASE_SERVICE_ROLE_KEY
+  });
+}
+export const config = { runtime: "nodejs" };
